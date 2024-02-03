6982533ca6d253045792546e056419774610cbe8
Iva Tomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-03 16:57:19 +0100

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,33 +1,32 @@
-import { UseChatHelpers } from 'ai/react'
+import React from 'react';
+import { UseChatHelpers } from 'ai/react';
 
-import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
-import { IconArrowRight } from '@/components/ui/icons'
+import { Button } from '@/components/ui/button';
+import { ExternalLink } from '@/components/external-link';
+import { IconArrowRight } from '@/components/ui/icons';
 
-import VoiceControlButton from '@/components/ui/VoiceControlButton'; // Import the VoiceControlButton component
+import VoiceControlButton from '@/components/ui/VoiceControlButton';
 
 const exampleMessages = [
   {
     heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    message: `What is a "serverless function"?`,
   },
   {
     heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    message: 'Summarize the following article for a 2nd grader: \n',
   },
   {
     heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
-]
+    message: `Draft an email to my boss about the following: \n`,
+  },
+];
 
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Loob!
-        </h1>
+        <h1 className="mb-2 text-lg font-semibold">Welcome to Loob!</h1>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
             <div className="flex items-center space-x-2" key={index}>
@@ -41,11 +40,12 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
               </button>
             </div>
           ))}
-        </div> 
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
 {/*         <p className="mb-2 leading-normal text-muted-foreground">
           This is an open source AI chatbot app template built with{' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}

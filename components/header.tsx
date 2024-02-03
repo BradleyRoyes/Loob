069c14395a6e31a7c1c1f148e72ba5caff04a73c
Bradley C Royes <77927301+BradleyRoyes@users.noreply.github.com>
2024-02-03 13:15:20 +0100

    Update header.tsx
    commented out the deploy button

--- components/header.tsx
+++ components/header.tsx
@@ -45,7 +45,7 @@ async function UserOrLogin() {
     </>
   )
 }
-
+/*
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
@@ -77,3 +77,4 @@ export function Header() {
     </header>
   )
 }
+*/

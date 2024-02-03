ab18f32509fb430036bc4c6e3598aa5da93f488f
Bradley C Royes <77927301+BradleyRoyes@users.noreply.github.com>
2024-02-03 13:44:24 +0100

    Update header.tsx
    removed commenting out

--- components/header.tsx
+++ components/header.tsx
@@ -45,7 +45,6 @@ async function UserOrLogin() {
     </>
   )
 }
-/*
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
@@ -77,4 +76,3 @@ export function Header() {
     </header>
   )
 }
-*/

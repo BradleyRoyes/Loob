8c04cc45e0df8208a1d58ef193bad93e5317e0bf
Iva Tomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-03 16:49:12 +0100

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -39,16 +39,18 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
         </p> */}
-        <div className="mx-auto max-w-2xl px-4">
-          <div className="rounded-lg border bg-background p-8">
-            <h1 className="mb-2 text-lg font-semibold">
-              Welcome to Loob!
-            </h1>
             <div className="mt-4 flex flex-col items-start space-y-2">
-              {exampleMessages.map((message, index) => (
-                <VoiceControlButton key={index} setInput={setInput} />
-              ))}
-            </div> 
+          {exampleMessages.map((message, index) => (
+            <div className="flex items-center space-x-2" key={index}>
+              <VoiceControlButton setInput={setInput} />
+              <button
+                className="h-auto p-0 text-base"
+                onClick={() => setInput(message.message)}
+              >
+                <IconArrowRight className="mr-2 text-muted-foreground" />
+                {message.heading}
+              </button>
+            </div>
           </div>
         </div>
       </div>
